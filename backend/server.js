8d84513a9e0274d69d6a6e24eaa094386711353a
Slake <inbox2034@gmail.com>
2021-01-10 19:01:42 +0500

    Cloudinary implemented on server side

--- backend/server.js
+++ backend/server.js
@@ -55,6 +55,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    `Server's running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   )
 );
